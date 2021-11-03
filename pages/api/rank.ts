5db0c43a4b0069dd20cb36b21ba3037f0e218994
Maz <maz.grasl@gmail.com>
2021-11-03 11:27:28 +0100

    Updated stats displayed (included win-loss, percentage and streaks), added more error handling, changed response status codes to 200 so that it does not interfere with nightbot

--- pages/api/rank.ts
+++ pages/api/rank.ts
@@ -3,7 +3,8 @@ import { NextApiRequest, NextApiResponse } from 'next';
 export default async function (req: NextApiRequest, res: NextApiResponse) {
 	let username: string = req.query.username as string;
 	if(!username) {
-		res.status(400).send('No username provided!');
+		// Nightbot only seems to print the message when urlfetch returns status 200, otherwise I would use a 4xx status here
+		res.status(200).send('No username provided!');
 		res.end();
 		return;
 	}
@@ -16,26 +17,47 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
 	  "teamSize": "1v1",
 	  "searchPlayer": "${username}",
 	  "page": 1,
-	  "count": 100
+	  "count": 1
 	}`;
 	
 	const response = await fetch(url, {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
+		cache: 'no-cache',
 		body: data
 	});
-	const json = await response.json();
+	console.log(response);
+	switch(response.status) {
+		case 200:
+			const json = await response.json();
+			console.log(json);
 
-	let found = false;
-    json.items.forEach(player => {
-      	if(!found && player.userName === username) {
-        	res.status(200).send(username + " is currently rank " + player.rank);
+			let found = false;
+			let responseString = JSON.stringify(json.items);
+			json.items.forEach(player => {
+				if(!found && player.userName === username) {
+					responseString = username + " is currently rank " + player.rank + ". They are " + player.wins + "-" + player.losses + " (" + player.winPercent + "%).";
+					if(player.winStreak != 0) {
+						responseString += " They are on a " + Math.abs(player.winStreak) + " game " + ((player.winStreak > 0) ? "winning" : "losing") + " streak.";
+					}
+					res.status(200).send(responseString);
+					res.end();
+					found = true;
+				}
+			});
+			if(!found) {
+				res.status(200).send("Could not find player " + username + " in " + json.count + " elements. Maybe the API has an internal error. Try again later.");
+				res.end();
+			}
+			break;
+		case 204:
+			res.status(200).send("Could not find player " + username);
 			res.end();
-			found = true;
-        }
-    });
-	if(!found) {
-		res.status(200).send("Could not find player " + username + " in " + json.count + " elements");
-		res.end();
+			break;
+		default:
+			console.log("UNEXPECTED ERROR!", username, response);
+			res.status(500).send("An unexpected error occurred: " + response.status + ". Yell at MazGrasl if you see him in chat.");
+			res.end();
+			break;
 	}
 }
