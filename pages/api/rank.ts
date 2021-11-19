d9c8112c233f775a33748886d42582f2f7dad3d0
Maz <maz.grasl@gmail.com>
2021-11-19 11:45:37 +0100

    Updated aoe4 API to use aoeiv.net, added ELO to result string

--- pages/api/rank.ts
+++ pages/api/rank.ts
@@ -2,49 +2,46 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
 	let username: string = req.query.username as string;
-	if(!username) {
+	let defaultUser: string = req.query.defaultuser as string;
+	if(!username && !defaultUser || (username.length <= 0 && defaultUser.length <= 0)) {
 		// Nightbot only seems to print the message when urlfetch returns status 200, otherwise I would use a 4xx status here
 		res.status(200).send('No username provided!');
 		res.end();
 		return;
 	}
-	var url = "https://api.ageofempires.com/api/ageiv/Leaderboard";
+	var url = "https://aoeiv.net/api/leaderboard?game=aoe4&leaderboard_id=17&count=1&start=1";
+	if(username && username.trim().length > 0) {
+		url += "&search=" + username;
+	} else if(defaultUser && defaultUser.length > 0) {
+		url += "&search=" + defaultUser;
+	}
+	console.log(url);
 
-	var data = `{
-	  "region": "7",
-	  "versus": "players",
-	  "matchType": "unranked",
-	  "teamSize": "1v1",
-	  "searchPlayer": "${username}",
-	  "page": 1,
-	  "count": 1
-	}`;
-	
 	const response = await fetch(url, {
-		method: 'POST',
+		method: 'GET',
 		headers: { 'Content-Type': 'application/json' },
-		cache: 'no-cache',
-		body: data
+		cache: 'no-cache'
 	});
 	console.log(response);
+
 	switch(response.status) {
 		case 200:
 			const json = await response.json();
 			console.log(json);
 
 			let found = false;
-			let responseString = JSON.stringify(json.items);
-			json.items.forEach(player => {
-				if(!found && player.userName === username) {
-					responseString = username + " is currently rank " + player.rank + ". They are " + player.wins + "-" + player.losses + " (" + player.winPercent + "%).";
-					if(player.winStreak != 0) {
-						responseString += " They are on a " + Math.abs(player.winStreak) + " game " + ((player.winStreak > 0) ? "winning" : "losing") + " streak.";
-					}
-					res.status(200).send(responseString);
-					res.end();
-					found = true;
+			if(json.leaderboard.length > 0) {
+				let player = json.leaderboard[0];
+				let winPercent = player.wins * 100 / player.games;
+				let streak = player.streak;
+				let responseString = player.name + " is currently rank " + player.rank + " with an ELO of " + player.rating + ". They are " + player.wins + "-" + player.losses + " (" + winPercent.toFixed() + "%).";
+				if(streak < -1 || streak > 1) {
+					responseString += " They are on a " + Math.abs(streak) + " game " + ((streak > 0) ? "winning" : "losing") + " streak.";
 				}
-			});
+				res.status(200).send(responseString);
+				res.end();
+				found = true; 
+			}
 			if(!found) {
 				res.status(200).send("Could not find player " + username + " in " + json.count + " elements. Maybe the API has an internal error. Try again later.");
 				res.end();
