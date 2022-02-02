d1f0007275fc6c6ad0044e4476120b3613ceea65
Maz <maz.grasl@gmail.com>
2022-02-02 10:09:58 +0100

    Added looping through all available APIs until it finds a match (or is out of APIs)

--- pages/api/rank.ts
+++ pages/api/rank.ts
@@ -1,5 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
+const OFFICIAL = "official";
+const AOEIV = "aoeiv";
+const AOE4WORLD = "aoe4world";
+
 export default async function (req: NextApiRequest, res: NextApiResponse) {
 	// username to search for
 	let username: string = req.query.username as string;
@@ -8,9 +12,15 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
 	// profile ID of a user, only used if username and defaultUser are empty
 	let profileID: string = req.query.profileid as string;
 	// which API to get the information from: official (ageofempires.com), aoeiv(.net), or aoe4world(.com)
+	const modes = [OFFICIAL, AOEIV, AOE4WORLD];
 	let mode: string = req.query.mode as string;
 	if(stringIsNullOrEmpty(mode)) {
-		mode = "aoe4world";
+		mode = AOE4WORLD;
+	}
+	if(modes.indexOf(mode) < 0) {
+		res.status(200).send('Mode ' + mode + ' is not supported!');
+		res.end();
+		return;
 	}
 	if(stringIsNullOrEmpty(username)) {
 		username = defaultUser;
@@ -21,42 +31,57 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
 		res.end();
 		return;
 	}
-	let response = await getResult(mode, username, profileID);
-	console.log(response);
+	let hasResult = false;
+	// remove the selected mode from the list
+	modes.splice(modes.indexOf(mode), 1);
+	// go through all the modes until we have a result
+	let currentMode = null;
+	while(!hasResult && modes.length > 0) {
+		if(!currentMode) {
+			currentMode = mode;
+		} else {
+			currentMode = modes.pop();
+		}
+		let response = await getResult(currentMode, username, profileID);
+		console.log(response);
+		
+		if(!response) {
+			continue;
+		}
+		switch(response.status) {
+			case 200:
+				const json = await response.json();
+				console.log(json);
 
-	switch(response.status) {
-		case 200:
-			const json = await response.json();
-			console.log(json);
-
-			let player = parseResult(json, mode, username, profileID);
-			if(player) {
-				let winPercent = player.winPercent;
-				let streak = player.streak;
-				let responseString = player.name + " is currently rank " + player.rank + " with an ELO of " + player.rating + ". They are " + player.wins + "-" + player.losses + " (" + winPercent.toFixed() + "%).";
-				if(streak < -1 || streak > 1) {
-					responseString += " They are on a " + Math.abs(streak) + " game " + ((streak > 0) ? "winning" : "losing") + " streak.";
+				let player = parseResult(json, currentMode, username, profileID);
+				if(player) {
+					let winPercent = player.winPercent;
+					let streak = player.streak;
+					let responseString = player.name + " is currently rank " + player.rank + " with an ELO of " + player.rating + ". They are " + player.wins + "-" + player.losses + " (" + winPercent.toFixed() + "%).";
+					if(streak < -1 || streak > 1) {
+						responseString += " They are on a " + Math.abs(streak) + " game " + ((streak > 0) ? "winning" : "losing") + " streak.";
+					}
+					hasResult = true;
+					res.status(200).send(responseString);
+					res.end();
+					return;
 				}
-				res.status(200).send(responseString);
-				res.end();
-			} else {
-				res.status(200).send("Could not find player " + username + ". Make sure you typed the name correctly and they have completed at least 10 matches.");
-				res.end();
-			}
-			break;
-		case 204:
-			res.status(200).send("Could not find player " + username);
-			res.end();
-			break;
-		case 500:
-			res.status(200).send("Remote API is having issues. Try again later.");
-			res.end();
-			break;
-		default:
-			console.log("UNEXPECTED ERROR!", username, response);
-			res.status(500).send("An unexpected error occurred: " + response.status + ".");
-			res.end();
-			break;
+				break;
+			case 204:
+				console.log("Could not find player " + username);
+				break;
+			case 500:
+				console.log("Remote API " + currentMode + " is having issues. Try again later.");
+				break;
+			default:
+				console.log("UNEXPECTED ERROR ", response.status, username, response);
+				break;
+		}
+	}
+	// if we still don't have a result, display an error message
+	if(!hasResult) {
+		res.status(200).send("Could not find player " + username + ". Make sure you typed the name correctly and they have completed at least 10 matches.");
+		res.end();
 	}
 }
 
@@ -67,7 +92,7 @@ function stringIsNullOrEmpty(str) {
 async function getResult(mode, username, profileID) {
 	let url;
 	let response;
-	if(mode == "official") {
+	if(mode == OFFICIAL) {
 		url = "https://api.ageofempires.com/api/ageiv/Leaderboard";
 		let data = `{
 		  "region": "7",
@@ -85,7 +110,7 @@ async function getResult(mode, username, profileID) {
 			body: data
 		});
 	}
-	if(mode == "aoeiv") {
+	if(mode == AOEIV) {
 		url = "https://aoeiv.net/api/leaderboard?game=aoe4&leaderboard_id=17&count=1&start=1";
 		if(!stringIsNullOrEmpty(username)) {
 			url += "&search=" + encodeURIComponent(username);
@@ -98,7 +123,7 @@ async function getResult(mode, username, profileID) {
 			cache: 'no-cache'
 		});
 	}
-	if(mode == "aoe4world") {
+	if(mode == AOE4WORLD) {
 		if(!stringIsNullOrEmpty(username)) {
 			url = "https://aoe4world.com/api/v0/leaderboards/qm_1v1?query=" + encodeURIComponent(username);
 		} else if(!stringIsNullOrEmpty(profileID)) {
@@ -115,7 +140,7 @@ async function getResult(mode, username, profileID) {
 }
 
 function parseResult(result, mode, username, profileID) {
-	if(mode == "official") {
+	if(mode == OFFICIAL) {
 		let searchField = "userName";
 		let searchTerm = username;
 		if(stringIsNullOrEmpty(username)) {
@@ -123,7 +148,7 @@ function parseResult(result, mode, username, profileID) {
 			searchField = "rlUserId";
 		}
 		let player = result.items.find(player => {
-			return player[searchField] == searchTerm;
+			return player[searchField].toLowerCase() == searchTerm.toLowerCase();
 		});
 		if(!player) {
 			return null;
@@ -138,7 +163,10 @@ function parseResult(result, mode, username, profileID) {
 			losses: player.losses
 		};
 	}
-	if(mode == "aoeiv") {
+	if(mode == AOEIV) {
+		if(result.leaderboard.length <= 0) {
+			return null;
+		}
 		let player = result.leaderboard[0];
 		let winPercent = player.wins * 100 / player.games;
 		return {
@@ -151,10 +179,13 @@ function parseResult(result, mode, username, profileID) {
 			losses: player.losses
 		};
 	}
-	if(mode == "aoe4world") {
+	if(mode == AOE4WORLD) {
 		let player;
 		let name;
 		if(!stringIsNullOrEmpty(username)) {
+			if(result.players.length <= 0) {
+				return null;
+			}
 			player = result.players[0];
 			name = player.name;
 		} else {
